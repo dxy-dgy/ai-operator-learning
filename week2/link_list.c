4ef06809ad50f0581b39f7c96c49ea1afbfb19cd
dxy-dgy <apdgy@icloud.com>
2025-12-30 18:13:54 +0800

    简单的单向链表头插尾插遍历测试

---
+++ week2/link_list.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node{
+    int val;
+    struct Node* next;
+} Node;
+void head_insert(Node* head, int val){
+    if (head == NULL){
+        return;
+    }
+    Node* new_node = malloc(sizeof(Node));
+    if (new_node == NULL){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    new_node->val = val;
+    new_node->next = head->next;
+    head->next = new_node;
+}
+void tail_insert(Node* head, int val){
+    if (head == NULL){
+        return;
+    }
+    Node* new_node = malloc(sizeof(Node));
+    if (new_node == NULL){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    new_node->val = val;
+    new_node->next = NULL;
+    Node* p = head;
+    while (p->next != NULL){
+        p = p->next;
+    }
+    p->next = new_node;
+}
+void print_all(Node* head){
+    if (head == NULL || head->next == NULL){
+        printf("当前为空链表！\n");
+        return;
+    }
+    Node* p = head->next;
+    int i = 1;
+    printf("以下为链表的值：\n");
+    while (p->next != NULL){
+        printf("第%d个值为%d\n", i, p->val);
+        p = p->next;
+        i+=1;
+    }
+    printf("第%d个值为%d\n", i, p->val);
+}
+int main(){
+    // 申请一个头节点
+    Node* head = malloc(sizeof(Node));
+    if (head == NULL){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    head->next = NULL;
+    print_all(head);
+    head_insert(head, 3);
+    print_all(head);
+    head_insert(head, 2);
+    head_insert(head, 1);
+    head_insert(head, 0);
+    print_all(head);
+    tail_insert(head, 4);
+    tail_insert(head, 5);
+    print_all(head);
+    return 0;
+}
